a0f022c4eabfd62e950a150dce439781b0de66b3
Sverker <maxime.attala@gmail.com>
2024-03-22 09:49:01 +0100

    fix(front): Fix import errors

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,6 +1,7 @@
-import { createRoot } from 'react-dom/client.js';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import React from 'react';
 
 createRoot(document.getElementById('root'))
   .render(
