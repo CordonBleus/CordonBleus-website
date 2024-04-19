0fdbf19537853ffcfb93ac700b99ecdf362618e2
Sverker <maxime.attala@gmail.com>
2024-04-19 16:50:59 +0200

    feat(api): update route, add credentials example

--- api/src/main.js
+++ api/src/main.js
@@ -1,12 +1,18 @@
 import express from 'express';
 
+import gMeetRouter from "./routes/gmeet"
+import gLoginRouter from "./routes/google-login";
+
 const app = express();
+
+app.use('/api/meet', gMeetRouter);
+app.use('/api/google', gLoginRouter);
 
 app.get('/', (_, res) => {
   res.send('Welcome to the API!');
 })
 
-if (import.meta.env.DEV) {
+if (import.meta.env.PROD) {
   app.listen(3000, () => {
     console.log('API ready !');
   });
