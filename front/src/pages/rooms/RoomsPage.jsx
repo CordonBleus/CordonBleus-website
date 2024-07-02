1e56e82fcac2f112092e6298645d06ea7b1675e9
Sverker <maxime.attala@gmail.com>
2024-07-02 23:00:21 +0200

    feat(api): call to login meet on create room

--- front/src/pages/rooms/RoomsPage.jsx
+++ front/src/pages/rooms/RoomsPage.jsx
@@ -3,6 +3,8 @@ import Header from "../../components/header/Header.jsx";
 import RoomsPageStyle from "./RoomsPage.module.css";
 import CardRoom from "../../components/cardRoom/CardRoom.jsx";
 import ListOfRoomCards from "../../components/listOfRoomCards/ListOfRoomCards.jsx";
+import Button from "../../components/button/button.jsx";
+import {useNavigate} from "react-router-dom";
 
 /**
  * Login Page
@@ -10,11 +12,18 @@ import ListOfRoomCards from "../../components/listOfRoomCards/ListOfRoomCards.js
  * @constructor
  */
 function RoomsPage({room}) {
-    return (
+
+  const handleClick = async () => {
+
+    location = import.meta.env.VITE_API_URI + '/api/google/login'
+  }
+
+  return (
         <section className={RoomsPageStyle.page}>
             <Header />
                 <h1 className={RoomsPageStyle.title}>Rooms</h1>
             <ListOfRoomCards  />
+            <Button text={"Create Room"} onClick={handleClick}/>
         </section>
     );
 }
