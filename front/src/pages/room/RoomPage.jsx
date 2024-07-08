523f370d31a44d4850f9f4095a607ff7ee3e0fef
Sverker <maxime.attala@gmail.com>
2024-07-08 15:22:24 +0200

    feat(front): get meeting uri from state in Room template

--- front/src/pages/room/RoomPage.jsx
+++ front/src/pages/room/RoomPage.jsx
@@ -1,16 +1,24 @@
-import React from "react";
 import Header from "../../components/header/Header.jsx";
 import RoomPageStyle from "./RoomPage.module.css";
 import RoomComponent from "../../components/room/RoomComponent.jsx";
+import {useLocation} from "react-router-dom";
+import PropTypes from "prop-types";
 
 function Room({ roomName }) {
+    const location = useLocation()
+    const meetingUrl = location.state.roomInfo.meetingUrl
+
     return (
         <section className={RoomPageStyle.page}>
             <Header />
             <h1 className={RoomPageStyle.title}>Room: {roomName}</h1>
-            <RoomComponent />
+            <RoomComponent meetingUrl={meetingUrl}/>
         </section>
     );
 }
+
+Room.propTypes = {
+    roomName: PropTypes.string.isRequired
+};
 
 export default Room;
