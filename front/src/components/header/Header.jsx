006790c8cf10cf081a2fcdca4201a88f01290ad4
Sverker <maxime.attala@gmail.com>
2024-07-13 17:29:18 +0200

    feat(front): dynamic button text in header

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -4,14 +4,25 @@ import styleHeader from "./Header.module.css"
 import logoHorizontal from "../../assets/logo/1_treansparente.png"
 import Button from "../button/button.jsx";
 function Header({user}) {
+    const logOut = () => {
+        localStorage.removeItem("googleToken");
+        window.location.href = "/";
+    }
+
+    const headerBtnText = () => {
+      if (localStorage.getItem("googleToken") != null) {
+        return "Log Out";
+      }
+      return "Log In";
+    }
+
     return (
         <header className={styleHeader.header}>
             <a href="" ><img src={logoHorizontal} className={styleHeader.logoheader}/></a>
             <input className={styleHeader.menubtn} type="checkbox" id="menubtn"/>
             <label className={styleHeader.menuIcon} htmlFor="menubtn"><span className={styleHeader.navicon}></span></label>
             <ul className={styleHeader.menu}>
-                <li><a href="#">Do you have an acccount ?</a></li>
-                <Button text={"Sigin up"}/>
+                <Button text={headerBtnText()} onClick={logOut}/>
             </ul>
         </header>
     );
