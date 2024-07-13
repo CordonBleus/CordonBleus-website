f5476f381415dbb21a4b55578dc8119b58a299ff
Sverker <maxime.attala@gmail.com>
2024-07-13 18:21:52 +0200

    fix(front): better style for room page

--- front/src/components/room/RoomComponent.jsx
+++ front/src/components/room/RoomComponent.jsx
@@ -1,13 +1,16 @@
 import PropTypes from "prop-types";
+import RoomComponentStyle from "./RoomComponent.module.css"
 
 function RoomComponent({meetingUrl}) {
     return (
         <div>
             <div>
-                <a href={meetingUrl} target="_blank"><p>Video meet</p></a>
-                <p>Recette</p>
-                <p>Ingrédients</p>
-                <p>Participants</p>
+              <div className={RoomComponentStyle.info}>
+                <p>Participants : {"N"}</p>
+                <p><a href={meetingUrl} target="_blank">Video meet</a></p>
+              </div>
+                <h2>Ingrédients</h2>
+                <h2>Recette</h2>
             </div>
         </div>
     );
