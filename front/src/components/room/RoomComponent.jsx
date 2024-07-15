c8cd595a9bfc98d677a7753636989bc17b731981
Sverker <maxime.attala@gmail.com>
2024-07-15 10:25:26 +0200

    feat(front): style for room component

--- front/src/components/room/RoomComponent.jsx
+++ front/src/components/room/RoomComponent.jsx
@@ -1,23 +1,35 @@
 import PropTypes from "prop-types";
 import RoomComponentStyle from "./RoomComponent.module.css"
 
-function RoomComponent({meetingUrl}) {
+function RoomComponent({meetingUrl, recipe}) {
     return (
         <div>
-            <div>
+            <img src={recipe.imageUrl}/>
+            <div className={RoomComponentStyle.container}>
               <div className={RoomComponentStyle.info}>
                 <p>Participants : {"N"}</p>
                 <p><a href={meetingUrl} target="_blank">Video meet</a></p>
               </div>
                 <h2>Ingrédients</h2>
+                <ul>
+                    {recipe.ingredients.map((ingredient) => (
+                        <li key={ingredient.id}>{ingredient.name}</li>
+                    ))}
+                </ul>
                 <h2>Recette</h2>
+                <ol>
+                    {recipe.steps.map((step, key) => (
+                        <li key={key}>{step}</li>
+                    ))}
+                </ol>
             </div>
         </div>
     );
 }
 
 RoomComponent.propTypes = {
-    meetingUrl: PropTypes.string
+    meetingUrl: PropTypes.string,
+    recipe: PropTypes.object
 }
 
 export default RoomComponent;
